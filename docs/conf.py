409a0345e6c133fd9f65f08c157a508589e0b32b
jtbabicz <jbabicz@stanford.edu>
2024-09-06 23:05:02 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,26 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
+#import os
+#import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+#sys.path.insert(0, os.path.abspath(".."))
 
-project = 'XSpecT'
-copyright = '2024, Leland B. Gee'
-author = 'Leland B. Gee'
-release = '0.1'
+#project = 'XSpecT'
+#copyright = '2024, Leland B. Gee'
+#author = 'Leland B. Gee'
+#release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+#extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+#templates_path = ['_templates']
+#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_theme = 'sphinx_rtd_theme'
+#html_static_path = ['_static']
